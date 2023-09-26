2215123e6764306b4fbbd8a416185784a54813eb
Scrub <43659841+Scott-Donaldson@users.noreply.github.com>
2023-09-26 15:22:16 +0100

    added tailwind

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { AuthProvider } from 'react-auth-kit'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider
+      authType='cookie'
+      authName='_qrcade'
+      cookieDomain={window.location.hostname}
+      cookieSecure={false}
+    >
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
 )
