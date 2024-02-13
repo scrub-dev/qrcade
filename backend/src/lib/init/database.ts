072e41fa77c095f8b1bb58e21d7babf6bb56d09b
Scrub <scott-donaldson@outlook.com>
2024-02-13 00:53:37 +0000

    Database model creation

--- backend/src/lib/init/database.ts
+++ backend/src/lib/init/database.ts
@@ -4,6 +4,7 @@ import iniConfig from '@config/InitConfig.json' assert {type: "json"}
 import { Log, LogType } from '@lib/logging/log.js'
 import { Database } from '@lib/database/database.js'
 import {ErrorHandler} from '@lib/errorhandler/ErrorHandler.js'
+import associations, { models } from 'src/models/associations.js'
 
 export const doesFileExist = (location: string, name: string) => {
     return fs.existsSync(location + name)
@@ -27,11 +28,22 @@ export const initialiseDatabase = async (location: string) => {
     try {
         Database.setInstance(location + config.DATABASE_NAME).then(() => {Log(`Database instance set...`, LogType.INI)})
 
-        Log(`Testing database connection`, LogType.INI)
+        Log(`Testing database connection`, LogType.DATABASE)
         ;(await Database.getInstance()).authenticate().then(() =>
-            Log(`Database connection test successful`, LogType.INI))
+            Log(`Database connection test successful`, LogType.DATABASE))
 
     } catch (err: unknown) {
+        let msg = "Unknown error occured"
+        if(err instanceof Error) msg = err.message
+        ErrorHandler.Error(msg)
+    }
+
+    try {
+        const database = await Database.getInstance()
+        models().forEach(m => m(database))
+        associations(database)
+        database.sync()
+    } catch (err) {
         let msg = "Unknown error occured"
         if(err instanceof Error) msg = err.message
         ErrorHandler.Error(msg)
