9cefddc67b857df6b6322d28a760c0123069a664
Scrub <scott-donaldson@outlook.com>
2024-02-23 20:44:39 +0000

    shortened scoreboard button

--- frontend/src/page/profile.tsx
+++ frontend/src/page/profile.tsx
@@ -6,9 +6,6 @@ export default () => {
 
     const user = useAuthUser() as any
 
-
-
-
     return (<>
     <div id="layout" className="flex flex-col h-screen text-white">
         <div id="banner" className="flex flex-col items-center justify-center w-screen bg-black py-3">
