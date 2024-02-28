536d3399ebffcde79bcba1a3917b15cbb410784e
Scrub <scott-donaldson@outlook.com>
2024-02-28 21:13:08 +0000

    admin controller functions for user management

---
+++ backend/src/server/controllers/AdminController.ts
@@ -0,0 +1,3 @@
+export const deleteUser = (userID: string) => {}
+export const resetDisplayName = (userID: string, newDisplayName: string) => {}
+export const resetPassword = (userID: string, newPassword: string) => {}
