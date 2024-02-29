d5006eb95dc2188d6466cf26c8b69008a5852dc7
Scrub <scott-donaldson@outlook.com>
2024-02-29 16:11:43 +0000

    Admin controller safety checks

--- backend/src/server/controllers/AdminController.ts
+++ backend/src/server/controllers/AdminController.ts
@@ -1,13 +1,58 @@
+import { sequelize } from "@src/lib/database/database"
+import { getUserByID } from "@src/lib/models/user/getUser"
 import { Request, Response } from "express"
+import JsonResponse from "../responses/JsonResponse"
+import { IUser } from "@src/models/user"
+import hashPassword from "@src/lib/user/hashPassword"
 
-export const deleteUser = (req: Request, res: Response) => {}
-export const reset = (req: Request, res: Response) => {}
-export const add = (req: Request, res: Response)  => {}
-export const remove = (req: Request, res: Response) => {}
+export const reset = async (req: Request, res: Response) => {
+    let paramToReset = req.params.param.toUpperCase()
+    let userID = req.params.userid
+
+    let user = (await getUserByID(userID)) as unknown as IUser
+    if(!user) return JsonResponse.NotFound(res).send()
+
+    switch(paramToReset){
+        default:
+            return JsonResponse.FieldNotSupported(res).send()
+        case "DISPLAYNAME":
+            resetDisplayName(userID, user.Username)
+            return JsonResponse.FieldUpdated(res, "displayname").send()
+        case "PASSWORD":
+            resetPassword(userID, user.Username)
+            return JsonResponse.FieldUpdated(res, "password").send()
+    }
+}
+
+export const deleteUser = async (req: Request, res: Response) => {
+    let user = (await getUserByID(req.params.userid)) as unknown as IUser
+    if(!user) return JsonResponse.NotFound(res).send()
+    await sequelize.models.Users.destroy({where: {UserID: req.params.userid}})
+}
+
+export const add = async (req: Request, res: Response)  => {
+    let paramToReset = req.params.param.toUpperCase()
+    let userID = req.params.userid
+
+    let user = (await getUserByID(userID)) as unknown as IUser
+    if(!user) return JsonResponse.NotFound(res).send()
+}
+
+export const remove = async (req: Request, res: Response) => {
+    let paramToReset = req.params.param.toUpperCase()
+    let userID = req.params.userid
+
+    let user = (await getUserByID(userID)) as unknown as IUser
+    if(!user) return JsonResponse.NotFound(res).send()
+}
 
 
-const resetDisplayName = (userID: string, newDisplayName: string) => {}
-const resetPassword = (userID: string, newPassword: string) => {}
+const resetDisplayName = async (userID: string, newDisplayName: string) => {
+    (await sequelize.models.Users.update({DisplayName: newDisplayName}, {where: {UserID : userID}}))
+}
+const resetPassword = async (userID: string, newPassword: string) => {
+    (await sequelize.models.Users.update({Passwd: hashPassword(newPassword)}, {where: {UserID : userID}}))
+}
 
 const addAdmin = (userID : string) => {}
 const removeAdmin = (userID : string) => {}
