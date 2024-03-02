dc34546e0ad9521557bbcb1cd3c6af63b0019bd5
Scrub <scott-donaldson@outlook.com>
2024-03-02 02:24:26 +0000

    Added buttons to frontend landing page

--- frontend/src/components/core/Button.tsx
+++ frontend/src/components/core/Button.tsx
@@ -6,7 +6,7 @@ export interface IButtonProps {
 
 
 export const defaultButtonStyle = "text-2xl px-4 py-1 bg-main rounded font-graffiti text-white hover:bg-main_light hover:shadow-lg hover:shadow-main"
-export const defaultButtonStyleAlt = "text-2xl px-4 py-1 bg-main rounded font-graffiti text-white bg-secondary_dark hover:bg-secondary hover:shadow-lg hover:shadow-main"
+export const defaultButtonStyleAlt = "text-2xl px-4 py-1 bg-main rounded font-graffiti text-white bg-secondary_dark hover:bg-secondary hover:shadow-lg hover:shadow-secondary_light"
 
 export default (props: IButtonProps) => {
     return (
--- frontend/src/page/root.tsx
+++ frontend/src/page/root.tsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom"
+import Button, { defaultButtonStyleAlt } from "../components/core/Button"
 import Loginout from "../components/login/loginout"
 import ViewScoreBtn from "../components/score/ViewScoreBtn"
 
 export default () => {
+
+    const nav = useNavigate()
+
     return (<>
     <div id="layout" className="flex flex-col h-screen text-white">
         <div id="banner" className="flex flex-wrap w-screen bg-black py-3">
@@ -13,12 +18,12 @@ export default () => {
                 <Loginout isRoot={true}/>
             </div>
         </div>
-        <div id="content-wrapper" className="bg-black flex-grow p-10 gap-2 flex flex-col md:px-[10%] lg:px-[20%] xl:px-[25%]">
+        <div id="content-wrapper" className="bg-black flex-grow pt-5 px-10 gap-2 flex flex-col md:px-[10%] lg:px-[20%] xl:px-[25%]">
             <div id="content1" className="rounded justify-center bg-main_light p-2 shadow-lg shadow-main_light hover:bg-main_dark hover:shadow-main_dark transition-all">
-                <p className="text-2xl">Welcome to QRCade!</p>
-                <p className="pt-1">QRCade is a simple, mobile gaming platform designed around open play!</p>
+                <p className="text-2xl text-center">Welcome to QRCade!</p>
+                <p className="pt-1 text-center">QRCade is a simple, mobile gaming platform designed around open play!</p>
                 <br></br>
-                <p className=" text-xl">Minimum Requirements:</p>
+                {/* <p className=" text-xl">Minimum Requirements:</p>
                 <div className="ml-10">
                     <ul className="list-disc ">
                         <li>A mobile phone with an internet connection.</li>
@@ -31,17 +36,15 @@ export default () => {
                             </div>
                         <li>Access to a printer if you need to print off your own QR Code or are running a game.</li>
                     </ul>
-                </div>
+                </div> */}
             </div>
-            {/* <div id="content2" className="rounded bg-main_light p-2 shadow-lg shadow-main_light hover:bg-main_dark hover:shadow-main_dark transition-all">
-                <p className="text-2xl">What is open play?</p>
-                <p className="pt-1">Remeber in primary school, where you would invent games on the playground with your own rules?</p>
-                <p className="">Its that!</p>
-                <br></br>
-                <p className="">You might now be asking yourself, how can I do open play with a mobile game?</p>
-                <p className="">Don't worry, the mobile app simply provides tools for you to still do this.</p>
-                <p className="">It provides a set of tools for you to build games around (of course they have to revolve around scanning a QR Code, sorry).</p>
-            </div> */}
+            <div className="flex flex-wrap items-center justify-center gap-2 pt-4">
+                <Button text={"About QRCade"} onClick={() => {nav("/")}} className={defaultButtonStyleAlt + " w-full md:w-max"}/>
+                <Button text={"How 2 Play"} onClick={() => {nav("/")}} className={defaultButtonStyleAlt + " w-full md:w-max "}/>
+                <a target='_blank' rel='noopener noreferrer' href="https://scrub-dev.com" className={defaultButtonStyleAlt + " w-full md:w-max text-center"}>My Website</a>
+                <a target='_blank' rel='noopener noreferrer' href="https://github.com/scrub-dev" className={defaultButtonStyleAlt + " w-full md:w-max text-center"}>My Projects</a>
+                <a target='_blank' rel='noopener noreferrer' href="https://ko-fi.com/scrubdev" className={defaultButtonStyleAlt + " w-full md:w-max text-center"}>Support Me</a>
+            </div>
         </div>
         <div id="footer" className="text-white bg-black">
             <p>QRCade © {new Date(Date.now()).getFullYear()}</p>
